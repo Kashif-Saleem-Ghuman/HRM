a8d1e38dd347801ad9a9a8a9eed7503047628e4d
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-03-03 16:56:02 +0530

    Console error handling

--- pages/myprofile/_id.vue
+++ pages/myprofile/_id.vue
@@ -980,29 +980,25 @@ export default {
   //     localStorage.getItem('tabSelected') === null ? 'Employee Profile' : localStorage.getItem('tabSelected')
   //   }
   // },
-  async created() {
-    // await this.users();
-    this.$store.dispatch("token/setActiveTab", "Employee Profile")
-      if (this.$route.params.id) {
-        this.id = this.$route.params.id;
-        await this.$store.dispatch("employee/setUser", this.id)
-      } else {
-        await this.$store.dispatch("employee/setActiveUser")
-        var users = this.getUser;
-        this.id = users.id
-        console.log(users.id, "Users")
-      }
-      console.log(this.$route.params.id, "route Param")
-      await this.$store.dispatch("employee/setActiveUser")
-        var users = this.getUser;
-        this.id = users.id
-        console.log(users.id, "Users")
-      await this.$store.dispatch("department/setDepartmentList")
-      this.departmentOptions = this.getDepartment;
-      this.form = this.getUser;
+  // async created() {
+  //   // await this.users();
+  //   this.$store.dispatch("token/setActiveTab", "Employee Profile")
+  //     if (this.$route.params.id) {
+  //       this.id = this.$route.params.id;
+  //       await this.$store.dispatch("employee/setUser", this.id)
+  //     } else {
+  //       await this.$store.dispatch("employee/setActiveUser")
+  //       var users = this.getUser;
+  //       this.id = users.id
+  //       console.log(users.id, "Users")
+  //     }
+  //     console.log(this.$route.params.id, "route Param")
+  //     await this.$store.dispatch("department/setDepartmentList")
+  //     this.departmentOptions = this.getDepartment;
+  //     this.form = this.getUser;
     
    
-  },
+  // },
   computed: {
     ...mapGetters({
       // userList: "users/GET_USERS_LIST",
@@ -1018,7 +1014,7 @@ export default {
 
   async mounted() {
     // var tabSelected = localStorage.getItem('tabSelected')
-    console.log(this.activeUserRole, "asjdfhakjsfhkasfhdkjshadfkjhsdkfjjk")
+    console.log("asjdfhakjsfhkasfhdkjshadaskjdashkdjhaksjdhkashdkjhkfkjhsdkfjjk")
     this.formOptions = SELECT_OPTIONS;
     this.switchLabelOrgSettings = this.form.useOrganizationSettings != null
       ? "Yes"

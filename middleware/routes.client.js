817d48b1466356ed48c2e844b3b092693f3d09f9
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-07-12 10:07:21 +0530

    Leave Vacation page Filters UI - done

--- middleware/routes.client.js
+++ middleware/routes.client.js
@@ -4,29 +4,30 @@ export default function routesCheck() {
     var userRole = localStorage.getItem("userRole");
     var pathPath = this.$router.history.current.fullPath;
     // Admin Role Routes
-    if (userRole === "ADMIN") {
-      if (pathPath === "/") {
-        this.$router.push('/dashboard/');
-      }
-      if (pathPath === "/myprofile") {
-        this.$router.push('/notFound');
-      }
-      // if (pathPath === "/dashboard") {
-      //   this.$router.push(notFound);
-      //   console.log(pagePath, "page path");
-      // }
-    } 
-    // Super Admin based Routes Check
-    else if(userRole=='SUPERADMIN'){
-      if (pathPath === "/myprofile") {
-        this.$router.push(notFound);
-      }
-    }
-    // User Role Routes
-    else if (userRole === "USER") {
-      if (pathPath === "/myprofile") {
-        this.$router.push(notFound);
-      }
-    }
+    this.$router.push('/dashboard/');
+    // if (userRole === "ADMIN") {
+    //   if (pathPath === "/") {
+    //     this.$router.push('/dashboard/');
+    //   }
+    //   if (pathPath === "/myprofile") {
+    //     this.$router.push('/notFound');
+    //   }
+    //   // if (pathPath === "/dashboard") {
+    //   //   this.$router.push(notFound);
+    //   //   console.log(pagePath, "page path");
+    //   // }
+    // } 
+    // // Super Admin based Routes Check
+    // else if(userRole=='USER'){
+    //   if (pathPath === "/myprofile") {
+    //     this.$router.push(notFound);
+    //   }
+    // }
+    // // User Role Routes
+    // else if (userRole === "USER") {
+    //   if (pathPath === "/myprofile") {
+    //     this.$router.push(notFound);
+    //   }
+    // }
   }
 }

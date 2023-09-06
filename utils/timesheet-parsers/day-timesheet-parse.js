9c992409fe470086e45adc9fd23ac8d47ac31923
kelvin <kelvinjara@hotmail.fr>
2023-09-06 13:46:24 -0400

    generate activity report from timers if there's timers

--- utils/timesheet-parsers/day-timesheet-parse.js
+++ utils/timesheet-parsers/day-timesheet-parse.js
@@ -3,10 +3,19 @@ import BaseTimesheetParser from "./base-timesheet-parser";
 export default class DayTimesheetParser extends BaseTimesheetParser {
   parse() {
     // naming hack here, BaseTimesheetParser takes timesheets as arguments, we could change it for "data"
-    const employee = this.timesheets
-    const { timeEntries, timers} = employee
+    const employee = this.timesheets;
+    const { timeEntries, timers } = employee;
 
-    const activityReport = this.generateActivityReportFromTimer(timers, timeEntries);
+    let activityReport;
+
+    if (timers?.length) {
+      activityReport = this.generateActivityReportFromTimer(
+        timers,
+        timeEntries
+      );
+    } else {
+      activityReport = this.generateActivityReport(timeEntries);
+    }
     return activityReport;
   }
 }

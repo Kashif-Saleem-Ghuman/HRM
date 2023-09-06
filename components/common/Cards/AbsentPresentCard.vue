f69c13af41328357f55c50c052c4d0890cae095c
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-09-06 17:25:53 +0530

    File upload for user role - done

---
+++ components/common/Cards/AbsentPresentCard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="d-flex">
+      <div class="info-card-leave-wrapper" style="width: 100%">
+        <div>
+          <label>{{ title }}</label>
+        </div>
+        <div class="info-card-items mt-05">
+          <div>
+            <div class="subheading">{{ item.numLabel }}</div>
+            <span>{{ item.numberData }}</span>
+            </div>
+          <div>
+            <progress-circle
+              :progressCount="item?.progress"
+              :progressTitle="item?.progreesTitle"
+              :progressPercentage="item?.progress + '%'"
+              :fill="fill"
+              v-show="item.progress == null ? '' : item.progress + '%'"
+              emptyfill="#f1f1f1"
+            ></progress-circle>
+          </div>
+        </div>
+        <div class="pt-1 d-block">
+          <div>
+            <bib-avatar src="https://placekitten.com/300/300"></bib-avatar>
+            <bib-avatar src="https://placekitten.com/300/300"></bib-avatar>
+            <bib-avatar src="https://placekitten.com/300/300"></bib-avatar>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "Card",
+    props: {
+        title: {
+        type: String,
+      },
+      hashItem: {
+        type: String,
+      },
+      item: {
+        type: Object,
+      },
+      buttonLable: {
+        type: String,
+      },
+      icon: {
+        type: String,
+      },
+      buttonVariant: {
+        type: String,
+      },
+    },
+    data() {
+      return {
+        fill: { gradient: ["#ffb700", "#47b801"] },
+      };
+    },
+    methods: {
+      buttonAction(item) {
+        alert("Called", item);
+      },
+    },
+  };
+  </script>
+  

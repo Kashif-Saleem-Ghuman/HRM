65a6a5ce9d91582a0a2ea54794c92c521f1c1583
kelvin <kelvinjara@hotmail.fr>
2023-09-20 15:17:58 -0400

    date selector style

--- components/common/DateSelector/BaseDateButton.vue
+++ components/common/DateSelector/BaseDateButton.vue
@@ -74,16 +74,29 @@ $dropdown-item-padding: 8px;
 
 .dropdown-menu {
   width: $date-btn-width;
+  min-height: $date-btn-height;
   background-color: $dropdown-background;
   position: absolute;
+  border: 1px solid #e7e7e7;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;;
   z-index: 10;
-
+  font-size: .8em;
+  font-weight: 200;
+  margin-top: -$date-btn-height;
+  padding-top: .5em;
+  padding-bottom: .5em;
   .dropdown-item {
+    display: flex;
+    align-items: center;
     padding: $dropdown-item-padding;
     cursor: pointer;
-
+    margin-left: .1em;
+    margin-right: .1em;
+    height: 2em;
+    
     &:hover {
       background-color: $dropdown-item-hover-bg;
+      border-radius: 5px;
     }
   }
 }

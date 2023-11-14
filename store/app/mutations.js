aac4fe7eff3aea33735adea225f4248b6f9fa302
kelvin <kelvinjara@hotmail.fr>
2023-11-14 09:03:27 -0500

    store app module

---
+++ store/app/mutations.js
@@ -0,0 +1,7 @@
+const mutations = {
+  SET_SEARCH_RESULTS(state, results) {
+    state.searchResults = results;
+  },
+};
+
+export default mutations;
---
+++ store/app/state.js
@@ -0,0 +1,5 @@
+const state = {
+  searchResults: null,
+};
+
+export default state;

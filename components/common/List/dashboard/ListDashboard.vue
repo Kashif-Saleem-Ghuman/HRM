3be029711c5c973733ba55ab0d288e569b0872ea
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-12-04 14:46:19 +0530

    HR Dashboard - Attendance status incorrect - done

--- components/common/List/dashboard/ListDashboard.vue
+++ components/common/List/dashboard/ListDashboard.vue
@@ -62,11 +62,11 @@
       <template #cell(status)="data">
         <div class="text-dark pl-1">
           <chips-list
-            :title="data.value.active == true ? 'Online' : 'Offline'"
+            :title="data.value?.timers[0]?.type ? 'Online' : 'Offline'"
             iconShow="iconShow"
             icon="add"
             :className="[
-              data.value.active
+              data.value?.timers[0]?.type
                 ? 'chip-list-wrapper__sucess'
                 : 'chip-list-wrapper__light',
             ]"

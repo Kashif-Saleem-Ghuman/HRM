bb19cdc3e21039bf8d4a7647c667bdb1f7d8d983
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-12-22 12:56:01 +0530

    404 not found on dev server

--- middleware/routes.client.js
+++ middleware/routes.client.js
@@ -1,37 +1,10 @@
 // import notFound from "../layouts/error.vue";
 export default function routesCheck() {
   if (process.client) {
-    // var userRole = localStorage.getItem("userRole");
     var pagePath = this.$router.history.current.fullPath;
-    console.log(pagePath, "pagePath")
     if (pagePath === "/") {
           this.$router.push('/dashboard/');
         }
-    // Admin Role Routes
-    // this.$router.push('/dashboard/');
-    // if (userRole === "ADMIN") {
-    //   if (pathPath === "/") {
-    //     this.$router.push('/dashboard/');
-    //   }
-    //   if (pathPath === "/myprofile") {
-    //     this.$router.push('/notFound');
-    //   }
-    //   // if (pathPath === "/dashboard") {
-    //   //   this.$router.push(notFound);
-    //   //   console.log(pagePath, "page path");
-    //   // }
-    // } 
-    // // Super Admin based Routes Check
-    // else if(userRole=='USER'){
-    //   if (pathPath === "/myprofile") {
-    //     this.$router.push(notFound);
-    //   }
-    // }
-    // // User Role Routes
-    // else if (userRole === "USER") {
-    //   if (pathPath === "/myprofile") {
-    //     this.$router.push(notFound);
-    //   }
-    // }
+  
   }
 }

d8d436de24c0f93637c916d36627fec625903037
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2023-12-22 14:28:27 +0530

    404 not found on dev server

--- utils/functions/api_call/employees.js
+++ utils/functions/api_call/employees.js
@@ -33,7 +33,7 @@ export async function getEmployee(payload) {
     return data
   } catch (err) {
     if (err.response.status === 500) {
-      return window.open('/not-found')
+      return window.open('/not-found',"_self")
       // return this.$nuxt.error({ statusCode: 500, message: err.message })
     }
     console.log(err)  

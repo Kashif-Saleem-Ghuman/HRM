bac6709ff9f93fab0184b6bd890e62fdb0582bd6
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-01-15 18:06:54 +0530

    Sheet editor url update in app switcher

--- utils/dataJson/app_wrap_data.js
+++ utils/dataJson/app_wrap_data.js
@@ -27,7 +27,7 @@ const data = () => {
         img: "editor",
         color: "orange",
         text: "Editor",
-        href: process.env.WEB_EDITOR_APP_URL,
+        href: process.env.WEB_SHEET_EDITOR_APP_URL,
       },
       {
         img: "table",

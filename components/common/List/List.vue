b0103c6ac9e9b24976f91b9fd2923387931d1355
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-01-24 21:09:30 +0530

    Sorting added on timesheet page suggested by Obuli

--- components/common/List/List.vue
+++ components/common/List/List.vue
@@ -52,7 +52,7 @@
             class="info_wrapper"
             style="width: 100%; cursor: pointer"
           >
-            <div class="title" v-tooltip="getEmployeeFullName(data.value)">
+            <div class="title" title="getEmployeeFullName(data.value)">
               {{ getEmployeeFullName(data.value) | truncate(16, "...") }}
             </div>
             <div class="description">

cc2aed255526fdcff6ddb852f691b84150c5ce6e
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-03-06 12:51:17 +0530

    Date validation added

--- utils/functions/api_call/on-leave-employee.js
+++ utils/functions/api_call/on-leave-employee.js
@@ -1,6 +1,9 @@
 import { createConfig } from "./config";
 import { hrmApiAxiosInstance } from "./hrm-api-axios-instance";
 export async function onLeaveEmployees({date}) {
+  if (!date) {
+    throw new Error("date is required for fetching on leave employess");
+  }
   try {
     const config = createConfig();
     config.params = {date}

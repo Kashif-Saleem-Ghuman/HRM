e838ea3aed7658b493caf0a8775f32ae984e986e
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-03-19 08:59:59 +0530

    Date format correction for timesheet - done

--- utils/constant/routes.js
+++ utils/constant/routes.js
@@ -1,4 +1,4 @@
 export const ADMIN_HOME_PATH = "/dashboard/";
 export const USER_HOME_PATH = "/time-attendance/attendance/";
 export const USER_WEEK_VIEW_PATH = "/time-attendance/attendance?view=week&start=";
-export const USER_WEEK_VIEW = "/time-attendance/attendance?view=week";
+// export const USER_WEEK_VIEW = "/time-attendance/attendance?view=week";

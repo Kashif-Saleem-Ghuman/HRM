4c3e65be0dcabfb2bdb023344f90f20758207f47
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-04-06 09:05:07 +0530

    Hard code token removed

--- plugins/validate-jwt.js
+++ plugins/validate-jwt.js
@@ -1,7 +1,7 @@
 import { redirectToLogin } from "../utils/functions/api_call/auth";
 
 export default async ({ store, $cookies }) => {
-  let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQeTdMRGR3cE9xMWUxWUtYIiwic3ViZSI6ImNoYXJhbi5wYWxAcXNzdGVjaG5vc29mdC5jb20iLCJzdWJzIjoiQUNUSVZFIiwic3ViYiI6Ik8zR1dwbWJrNWV6Sm40S1IiLCJzdWJicyI6IkNMSUVOVCIsInN1YnIiOiJBRE1JTiIsInN1YmMiOiJVbml0ZWQgU3RhdGVzIiwiZW52IjoiZGV2IiwiaWF0IjoxNzEwODIwMDU0NzI3LCJleHAiOjE3MTg1OTYwNTQ3MjcsImp0aSI6ImE5NTJmMmIzLWRmZTAtNDE3Mi1hN2Y2LWM1YWE3MzAwMjEwNSJ9.RUAr6U5r6g8FyNcwHM43IquzxYya0ee4r_eL4xVI7ig';
+  let token = $cookies.get(process.env.SSO_COOKIE_NAME);
 
   if (!token) {
     return redirectToLogin();

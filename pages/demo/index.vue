2ddc11a6a2826c2c3759fdd65c91153609935e8f
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-04-16 09:29:40 +0530

    demo page for testing

---
+++ pages/demo/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="people-action-wrapper">  
+      <div class="d-flex justify-between align-center bottom_border_wrapper">
+        <section-header-left title="Demo page"></section-header-left>
+        <download-file></download-file>
+      </div>
+    </div>
+  </template>
+  <script>
+  </script>
+  

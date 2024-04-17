feaa07d98422cd88b772cbae9eb85981dd026b17
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-04-17 12:56:18 +0530

    Attendance - incorrect total hours has been fixed

--- utils/functions/time.js
+++ utils/functions/time.js
@@ -5,7 +5,14 @@ export const formatHoursToHHMM = (hours) => {
     hours = 0;
   }
 
-  return Duration.fromObject({ minutes: hours * 60 }).toFormat("hh:mm");
+  // return Duration.fromObject({ minutes: hours * 60 }).toFormat("hh:mm");
+  // added new changes to fix the minus value isSecureContext
+  const duration = Duration.fromObject({ minutes: Math.abs(hours * 60) }); // Use Math.abs() to handle negative hours correctly
+
+  const sign = hours < 0 ? "-" : ""; // Determine sign based on the sign of hours
+
+  return `${sign}${duration.toFormat("hh:mm")}`;
+
 };
 
 export function calculateTimeDifferenceInMinutes(startTime, endTime) {

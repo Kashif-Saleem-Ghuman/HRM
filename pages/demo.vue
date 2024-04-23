654e4e2fb348beee9c6aa827da19fec6467cf0f9
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-04-23 17:49:00 +0530

    Code refactor

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,29 +1,23 @@
 <template>
-    <div>
-      <div v-if="isAdmin">
+  <div>
     <download-file></download-file>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    
-    data() {
-      return {
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    isAdmin() {
+      return this.$store.state.token.isAdmin;
     },
-    computed: {
-      isAdmin() {
-        return this.$store.state.token.isAdmin
-      },
-      isUser() {
-        return this.$store.state.token.isUser
-      },
+    isUser() {
+      return this.$store.state.token.isUser;
     },
-    
-  };
-  </script>
-  
-  <style lang="scss" scoped></style>
-  
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

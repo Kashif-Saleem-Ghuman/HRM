89ba88e65fa1d50fb7ec2a7157ac20dcd2a6bd07
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-05-08 11:34:28 +0530

    Tabs update issue on back button fixed

--- utils/constant/routes.js
+++ utils/constant/routes.js
@@ -2,3 +2,4 @@ export const ADMIN_HOME_PATH = "/dashboard/";
 export const USER_HOME_PATH = "/time-attendance/attendance/";
 export const USER_WEEK_VIEW_PATH = "/time-attendance/attendance/?view=week&start="
 export const LEAVE_REQUEST_VIEW_PATH = "/leaves-and-vacations/dashboard/?request=";
+export const NY_PROFILE_VIEW_PATH = "/profile";

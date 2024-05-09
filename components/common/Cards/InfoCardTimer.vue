e86b0390ed4a08339625f4ebe7b648d62bf79fd2
Shahinur_Rahman <coredeveloper.2013@gmail.com>
2024-05-09 17:36:54 +0600

    Remove event listener when unmount

--- components/common/Cards/InfoCardTimer.vue
+++ components/common/Cards/InfoCardTimer.vue
@@ -100,17 +100,10 @@ export default {
         date: new Date().toISOString(),
       });
     }
-
-    document.addEventListener("visibilitychange",  () => {
-      if (document.hidden) {
-        this.$store.commit("timeattendance/SET_IS_TIMER_RUNNING", {
-          status: false,
-        });
-        this.clearChronometerInterval();
-      } else {
-        this.startTimerInterval();
-      }
-    });
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
+  },
+  beforeDestroy() {
+    document.removeEventListener("visibilitychange", this.handleVisibilityChange);
   },
   methods: {
     close() {
@@ -146,6 +139,17 @@ export default {
       }
     },
 
+    handleVisibilityChange() {
+
+        if (document.hidden) {
+          this.$store.commit("timeattendance/SET_IS_TIMER_RUNNING", {
+            status: false,
+          });
+          this.clearChronometerInterval();
+        } else {
+          this.startTimerInterval();
+        }
+    }
 
   },
   computed: {

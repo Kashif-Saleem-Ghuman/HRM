277ead970d257cd47db060f49070645ad13b5fba
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-07-23 12:07:17 +0530

    Lib Update

--- pages/people/index.vue
+++ pages/people/index.vue
@@ -89,15 +89,6 @@ export default {
       this.employees = employees;
       this.loading = false;
     },
-    sendMeet() {
-      window.open("https://dev-connect.business-in-a-box.com/", "_blank");
-    },
-    sendMessage() {
-      window.open(
-        "https://dev-chat.business-in-a-box.com/directs/" + this.form.userId,
-        "_blank"
-      );
-    },
     handleClickOutside() {
       this.slideClass = "slide-out";
       setTimeout(() => {

7d0215043dc1cea20f102ae0c9e16d8acd80fd54
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-08-13 10:50:07 +0530

    Bib bib-datetime-picker dark/light theme issue has been resolved

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,9 +1,10 @@
 import { USER_ROLES } from "../utils/constant/Constant";
 import { ADMIN_HOME_PATH, USER_HOME_PATH } from "../utils/constant/routes";
 
-export default async function ({ route, redirect, store }) {
+export default async function ({ route, redirect, store, app }) {
   const { path } = route;
   let userRole = localStorage.getItem("userRole");
+  await app.$isThemeCheck();
 
   if (!userRole) {
     userRole = store.state.token.isAdmin ? USER_ROLES.ADMIN : USER_ROLES.USER;

6ba82ed15c95389f724b689de65f1be3068ef9ce
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-08-21 16:56:01 +0530

    Merge with main

--- plugins/validate-jwt.js
+++ plugins/validate-jwt.js
@@ -1,7 +1,7 @@
 import { redirectToLogin } from "../utils/functions/api_call/auth";
 
 export default async ({ store, $cookies }) => {
-  let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3TkpBUGRSNlJaemRHeU9YIiwic3ViZSI6ImNjY3Rlc3RAbWFpbGluYXRvci5jb20iLCJzdWJzIjoiQUNUSVZFIiwic3ViYiI6InFMa3pQZHlQMGpXYlFyb3YiLCJzdWJicyI6IkNMSUVOVCIsInN1YnIiOiJVU0VSIiwic3ViYyI6IkNhbmFkYSIsImVudiI6ImRldiIsImlhdCI6MTcyNDIzODUwNDkxNiwiZXhwIjoxNzMyMDE0NTA0OTE2LCJqdGkiOiJmMmEzZjI2Mi05MjcxLTQ2MTItOTI3Ni1lNWJmOTIzNWM3YTgifQ.tTW2jYLT0O7iOYnCBTpN-wSpx517-3YQ3LcdWkuTH94';
+  let token = $cookies.get(process.env.SSO_COOKIE_NAME);
 
   if (!token) {
     return redirectToLogin();

ed6b4e39fe6c063acf7f4f6a3bfaf2423891ba1b
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-09-11 13:34:28 +0530

    when user click on a date (row) should take user to Day view - done

--- components/common/Cards/InfoCardLeaveVacation.vue
+++ components/common/Cards/InfoCardLeaveVacation.vue
@@ -159,9 +159,6 @@ export default {
   computed: {
     getpercentageValue() {
       const totalLeave = this.daysUsed + this.scheduledDays;
-      if(totalLeave > this.totalAllowance){
-        return '0';
-      }
       this.progressKey += 1;
       if (this.totalAllowance !== 0) {
         const percentage = Math.round((totalLeave / this.totalAllowance) * 100);

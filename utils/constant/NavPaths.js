e534dd06717dab152aa8e5b93aa75c1d8e79e8be
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-09-11 16:03:38 +0530

    Leave & Vacation tab should be highlighted when a user is on this tab - done

--- utils/constant/NavPaths.js
+++ utils/constant/NavPaths.js
@@ -1,29 +1,52 @@
 export const navigationPaths = [
   {
-    name: "leaveAndVacationEmployeePaths",
+    name: "leaveAndVacationPaths",
     paths: [
+      "/leaves-and-vacations/upcoming/",
+      "/leaves-and-vacations/leavelist/",
       "/leaves-and-vacations/dashboard/",
+      "/leaves-and-vacations/pendingrequest/",
     ],
-    defaultPath: "/leaves-and-vacations/dashboard/",
+    defaultPath: "/leaves-and-vacations/upcoming/",
   },
   {
-    name: "timeAttendanceEmployeePaths",
+    name: "timeAttendancePaths",
     paths: [
-      "/home/?view=day",
-      "/home/?view=week",
-      "/home/?view=month",
+      "/time-attendance/attendance/",
+      "/time-attendance/timesheets/",
+      "/time-attendance/month/",
+      "/time-attendance/year/",
+      "/time-attendance/pending/",
+      "/time-attendance/past-due/",
     ],
-    defaultPath: "/home/",
+    defaultPath: "/time-attendance/attendance/",
   },
   {
-    name: "myTimesheets",
+    name: "reportsPaths",
     paths: [
-      "/my-timesheet/?view=day",
-      "/my-timesheet/?view=week",
-      "/my-timesheet/?view=month",
+      "/reports/leave-report/",
+      "/reports/leave-summary/",
+      "/reports/timesheet-report/",
     ],
-    defaultPath: "/my-timesheet/",
+    defaultPath: "/reports/leave-report/",
   },
+  {
+    name: "peoplePath",
+    paths: [
+      "/profile/:id/employee-profile-tab",
+      "/profile/:id/employment-information-tab",
+      "/profile/:id/time-attendance-profile-tab",
+      "/profile/:id/leave-vacations-profile-tab",
+      "/profile/:id/files-profile-tab",
+      "/profile/:id/time-attendance-profile-tab?view=day",
+      "/profile/:id/time-attendance-profile-tab?view=week",
+      "/profile/:id/time-attendance-profile-tab?view=month",
+    ],
+    defaultPath: "/people/",
+  },
+];
+
+export const navigationPathsAdmin = [
   {
     name: "leaveAndVacationPaths",
     paths: [

e7b97231033e14d90c672ca4ef39516adf2e62e2
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-09-12 17:43:19 +0530

    Button Variant change only for rejected timesheets

--- components/common/List/Timesheet-Table-Footer/index.vue
+++ components/common/List/Timesheet-Table-Footer/index.vue
@@ -19,7 +19,7 @@
             class=""
           ></chips> -->
         </div>
-        <leave-status v-else :leaveStatusData="status"   @click="buttonClicked"></leave-status>
+        <leave-status v-else :leaveStatusData="status"   @click="buttonClicked" :buttonVarintChange="true"></leave-status>
 
         <!-- <bib-button
           :icon="getStatusIcon()"
@@ -77,7 +77,7 @@
             class=""
           ></chips> -->
         </div>
-        <leave-status v-else :leaveStatusData="status" :timesheet="true"  @click="buttonClicked"></leave-status>
+        <leave-status v-else :leaveStatusData="status" :timesheet="true"  @click="buttonClicked" :buttonVarintChange="true"></leave-status>
 
         <!-- <bib-button
           :icon="getSubmitIcon()"

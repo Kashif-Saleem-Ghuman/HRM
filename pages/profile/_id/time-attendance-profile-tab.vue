0a21e6c44bed45c01022ea8121b86a57bfcaa8ee
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-09-17 13:40:13 +0530

    Week total value fixe on month view

--- pages/profile/_id/time-attendance-profile-tab.vue
+++ pages/profile/_id/time-attendance-profile-tab.vue
@@ -363,12 +363,10 @@ export default {
         await getWeekTimesheets(weekRange)
       ).parse("week");
       this.weekDataActivityReports = weekData.activityReports || [];
-      const totalTimeInSeconds = Math.round(weekData.total * 3600);
-      this.weekDataTotalWork = formatTime(totalTimeInSeconds, false);
-      // this.weekDataTotalWork = formatTime(
-      //   (weekData.total || 0) * 60 * 60,
-      //   false
-      // );
+      this.weekDataTotalWork = formatTime(
+        (weekData.total || 0) * 60 * 60,
+        false
+      );
       this.weekDataStatus = weekData.status || "";
       this.timesheetId = weekData.id || "";
       this.loading = false;

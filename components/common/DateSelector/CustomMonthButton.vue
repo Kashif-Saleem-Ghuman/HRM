871b074cbdfbfa715a104bca210b7914349ba32d
charanpalqss <113195348+charan-pal@users.noreply.github.com>
2024-12-12 11:58:48 +0530

    Code formatted on month dropdown

--- components/common/DateSelector/CustomMonthButton.vue
+++ components/common/DateSelector/CustomMonthButton.vue
@@ -2,14 +2,32 @@
   <div>
     <div class="dropdown-chip-menu">
       <div class="position-relative" @mouseleave="handleMouseLeave">
-        <bib-button :label="selected" :variant="themeButtonVariant" v-click-outside="clickOutside"
-          :icon-right="iconName" @click="toggleDropdown" @click-right-icon="resetMonth"></bib-button>
-        <div class="menu-items chip-wrapper-com" style="left: 0;">
+        <bib-button
+          :label="selected"
+          :variant="themeButtonVariant"
+          v-click-outside="clickOutside"
+          :icon-right="iconName"
+          @click="toggleDropdown"
+          @click-right-icon="resetMonth"
+        ></bib-button>
+        <div class="menu-items chip-wrapper-com" style="left: 0">
           <div v-if="isDropdownOpen" class="chip-wrapper-inner">
-            <div v-for="(item, index) in getMonthsOptions" :key="index" @click="selectOption(item)"
-              class="cursor-pointer" :class="['cursor-pointer', { 'disabled-opacity': isDisabled(item) }]">
-              <bib-button :label="item.label" :variant="isLightThemeCheck ? 'light' : 'dark'" size="lg"
-                class="pr-05 mb-05 w-100"></bib-button>
+            <div
+              v-for="(item, index) in getMonthsOptions"
+              :key="index"
+              @click="selectOption(item)"
+              class="cursor-pointer"
+              :class="[
+                'cursor-pointer',
+                { 'disabled-opacity': isDisabled(item) },
+              ]"
+            >
+              <bib-button
+                :label="item.label"
+                :variant="isLightThemeCheck ? 'light' : 'dark'"
+                size="lg"
+                class="pr-05 mb-05 w-100"
+              ></bib-button>
             </div>
           </div>
         </div>
@@ -20,7 +38,10 @@
 
 <script>
 import { MONTHS_LABEL_VALUE } from "../../../utils/constant/Calander";
-import { MONTH_SELECTOR_DEFAULT, MONTH_SELECTOR_ICONS } from "../../../utils/constant/Constant";
+import {
+  MONTH_SELECTOR_DEFAULT,
+  MONTH_SELECTOR_ICONS,
+} from "../../../utils/constant/Constant";
 import BaseDateButton from "./BaseDateButton.vue";
 import { DateTime } from "luxon";
 export default {
@@ -46,18 +67,23 @@ export default {
   computed: {
     iconName() {
       if (this.value === MONTH_SELECTOR_DEFAULT.value) {
-        return MONTH_SELECTOR_ICONS.ARROW_DOWN
+        return MONTH_SELECTOR_ICONS.ARROW_DOWN;
       }
       return MONTH_SELECTOR_ICONS.CLOSE;
     },
     getMonthsOptions() {
       if (this.isCurrentYear(this.year)) {
-        return this.options.slice(0, this.options.findIndex((option) => option.value == DateTime.now().month) + 1)
+        return this.options.slice(
+          0,
+          this.options.findIndex(
+            (option) => option.value == DateTime.now().month
+          ) + 1
+        );
       }
       return this.options;
     },
     isFromTimesheetViewCard() {
-      return this.$store.state.timeattendance.viewFrom
+      return this.$store.state.timeattendance.viewFrom;
     },
   },
 
@@ -78,7 +104,11 @@ export default {
       this.isDropdownOpen = false;
     },
     setDefaultValue() {
-      if (this.showFullYearList || this.isFromTimesheetViewCard || this.monthPickerDisabled) {
+      if (
+        this.showFullYearList ||
+        this.isFromTimesheetViewCard ||
+        this.monthPickerDisabled
+      ) {
         this.setDefaultMonth();
       } else {
         const month = new Date().getMonth();
@@ -114,7 +144,7 @@ export default {
       if (oldVal != null && this.isCurrentYear(newVal)) {
         this.setDefaultValue();
       }
-    }
+    },
   },
 };
 </script>
